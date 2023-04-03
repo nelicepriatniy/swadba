076078112f9bf0d11bf9823dcf7bada830775897
nelicepriatniy <92029297+nelicepriatniy@users.noreply.github.com>
2023-04-04 00:57:03 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,45 +1,89 @@
 
 //анимации при скроле
-let windowHeight = window.innerHeight;
-let animBlocks = document.querySelectorAll('.animate');
-let windowScroll = window.scrollY + windowHeight;
 
-function getPosition(element) {
-    var yPosition = 0;
 
-    while(element) {
-        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
-        element = element.offsetParent;
+
+if (window.innerWidth > 1100) {
+    let windowHeight = window.innerHeight;
+    let animBlocks = document.querySelectorAll('.animate');
+    let windowScroll = window.scrollY + windowHeight;
+
+    function getPosition(element) {
+        var yPosition = 0;
+
+        while (element) {
+            yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+            element = element.offsetParent;
+        }
+
+        return yPosition;
     }
 
-    return yPosition;
-}
+    window.onscroll = () => {
+        animBlocks.forEach(el => {
+            windowScroll = window.scrollY + windowHeight;
+            let topoffset = getPosition(el);
+            if (topoffset < windowScroll) {
+                el.classList.add('active');
+            }
+        })
+    }
 
-window.onscroll = ()=>{
-    animBlocks.forEach(el=>{
+    animBlocks.forEach(el => {
         windowScroll = window.scrollY + windowHeight;
         let topoffset = getPosition(el);
-        if(topoffset < windowScroll) {
+        if (topoffset < windowScroll) {
             el.classList.add('active');
         }
     })
-}
 
-animBlocks.forEach(el=>{
-    windowScroll = window.scrollY + windowHeight;
-    let topoffset = getPosition(el);
-    if(topoffset < windowScroll) {
-        el.classList.add('active');
+} else {
+    let windowHeight = window.innerHeight;
+    let animBlocks = document.querySelectorAll('.animate');
+    let windowScroll = window.scrollY + windowHeight;
+
+    animBlocks.forEach(el => {
+        el.classList.add('active')
+    })
+
+    function getPosition(element) {
+        var yPosition = 0;
+
+        while (element) {
+            yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+            element = element.offsetParent;
+        }
+
+        return yPosition;
     }
-})
+
+    window.onscroll = () => {
+        animBlocks.forEach(el => {
+            windowScroll = window.scrollY + windowHeight;
+            let topoffset = getPosition(el);
+            if (topoffset < windowScroll) {
+                el.classList.add('active');
+            }
+        })
+    }
+
+    animBlocks.forEach(el => {
+        windowScroll = window.scrollY + windowHeight;
+        let topoffset = getPosition(el);
+        if (topoffset < windowScroll) {
+            el.classList.add('active');
+        }
+    })
+
+}
 
 //переключение табов в портфолио 
 
 let portfolioTabBtns = document.querySelectorAll('.portfolio__list__item');
 
-portfolioTabBtns.forEach(el=>{
-    el.onclick = ()=>{
-        portfolioTabBtns.forEach(el =>{
+portfolioTabBtns.forEach(el => {
+    el.onclick = () => {
+        portfolioTabBtns.forEach(el => {
             el.classList.remove('active')
         })
         el.classList.add('active')
@@ -49,9 +93,9 @@ portfolioTabBtns.forEach(el=>{
 //активация радио 
 
 let q1label = document.querySelectorAll('.radiobtn');
-q1label.forEach(el=>{
-    el.onclick = ()=>{
-        q1label.forEach(el=>{
+q1label.forEach(el => {
+    el.onclick = () => {
+        q1label.forEach(el => {
             el.classList.remove('active')
         })
         el.classList.add('active')
@@ -61,8 +105,8 @@ q1label.forEach(el=>{
 //активация чекбоксов 
 
 let checkboxbtn = document.querySelectorAll('.checkbtn');
-checkboxbtn.forEach(el=>{
-    el.onclick = ()=>{
+checkboxbtn.forEach(el => {
+    el.onclick = () => {
         el.classList.toggle('active')
     }
 })
@@ -76,17 +120,17 @@ let beguStrock = document.querySelector('.muwe-stick-block');
 let beguStrockTransform = 80;
 let numOfQuest = document.querySelector('.stepHere');
 
-nextBtns.forEach(button=>{
-    button.onclick = ()=>{
-        if(activeQuizBlockIndex < quizChangeBlocks.length -1) {
-            activeQuizBlockIndex ++;
-            quizChangeBlocks.forEach(el=>{
+nextBtns.forEach(button => {
+    button.onclick = () => {
+        if (activeQuizBlockIndex < quizChangeBlocks.length - 1) {
+            activeQuizBlockIndex++;
+            quizChangeBlocks.forEach(el => {
                 el.classList.remove('active')
             })
             quizChangeBlocks[activeQuizBlockIndex].classList.add('active');
             beguStrockTransform = beguStrockTransform - 20;
             // beguStrock.style.transform = 'translateX(- '+ beguStrockTransform +'%)';
-            beguStrock.style.transform = 'translateX(-'+ beguStrockTransform+'%)';
+            beguStrock.style.transform = 'translateX(-' + beguStrockTransform + '%)';
             numOfQuest.innerHTML = activeQuizBlockIndex + 1;
         }
     }
@@ -98,10 +142,33 @@ nextBtns.forEach(button=>{
 let textIn = document.querySelectorAll('.textIn');
 
 
-textIn.forEach(el=>{
+textIn.forEach(el => {
     let id = el.getAttribute('data-for')
     let block = document.querySelector(id)
-    el.addEventListener('keyup', function(){
+    el.addEventListener('keyup', function () {
         block.value = el.value
     })
 })
+
+//открытие портфолио в мобилке
+
+let openBtn = document.querySelector('.moreport')
+let portfolioItems = document.querySelectorAll('.portfolioPhoto')
+
+openBtn.onclick = () => {
+    portfolioItems.forEach((el) => {
+        el.classList.remove('mob-notactive')
+    })
+
+    openBtn.classList.add('notactive')
+}
+
+//мобильный слайдер в отзывах
+
+const swiper = new Swiper('.rew-slider', {
+    navigation: {
+        nextEl: '.rew-slider-next',
+        prevEl: '.rew-slider-prev',
+    },
+    spaceBetween: 50,
+});
